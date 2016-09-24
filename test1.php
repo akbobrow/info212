dcc38a15c7d362567678e67897f43487a5a3191b
burntoutmatch <akbobrow@gmail.com>
2016-09-24 18:58:35 +0200

    Update and rename test2.php to test1.php

---
+++ test1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>THIS IS THE TEST 1 PAGE</title>
+		
+	</head>
+	<body>
+	<p>THIS IS THE test 2 page. Pleaseee work!!</p>
+	<a href="http://htmlpreview.github.io/?https://github.com/burntoutmatch/info212/blob/master/test2.html">CLICK HERE TO GO BACK TO THE TEST PAGE</a>
+	
+		
+	</body>
+</html>
