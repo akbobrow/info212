1f1d503828b8cb0b379c58be01c133f6535f7818
burntoutmatch <akbobrow@gmail.com>
2016-09-24 18:36:03 +0200

    Add files via upload

---
+++ test2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>THIS IS THE TEST 2 PAGE</title>
+		
+	</head>
+	<body>
+	<p>THIS IS THE test 2 page. Pleaseee work!!</p>
+	<a href="test.php">CLICK HERE TO GO BACK TO THE TEST PAGE</a>
+	
+		<h1>Echo</h1>
+		
+		<?php echo "This is another page"; ?>
+		
+	</body>
+</html>
